f036e3f87ffb1c02261604ac271762ca216b783c
LakshShah <87525400+LakshShah@users.noreply.github.com>
2021-12-01 15:03:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var firstname= "Ranbir" ;
+var lastname= "Kapoor" ;
+console.log(firstname);
+console.log(lastname);
+var name = firstname.concat(lastname);
+var a=10;
+var b=5
+console.log(a);
+console.log(b);
+var add=(a+b);
+console.log(add);
